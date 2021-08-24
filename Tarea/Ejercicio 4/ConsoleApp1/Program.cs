158ecc2831c75f14e467d2ed6320527110882c27
Tomas Mandziuck <tomas.mandziuck@gmail.com>
2021-08-24 18:26:16 -0300

    24/08

---
+++ Tarea/Ejercicio 4/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int elNumero=0, i, j,contador=0, suma=0;
+
+            do 
+            {
+                elNumero++;
+                suma = 0;
+                for (i = 1; i < elNumero; i++)
+                {
+                    if (elNumero%i==0)
+                    {
+                        suma += i;
+                    }
+                    
+                }
+                if (suma == elNumero)
+                {
+                    Console.WriteLine("Numero perfecto{0}\n", elNumero);
+                    contador++;
+                }
+
+            } while(contador<4);
+           
+        }
+    }
+}
