0831d3ec62a647f7b9a7fbbc4c3bb0ee92834600
Tomas Mandziuck <tomas.mandziuck@gmail.com>
2021-08-24 16:23:38 -0300

    24/08

---
+++ Trabajo en clase/Ejercicios de primera clase/Ejercicio Clase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio_Clase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese nombre");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("ingrese edad");
+            string edad = Console.ReadLine();
+
+            int edadPeroNumero = int.Parse(edad);
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.BackgroundColor = ConsoleColor.Gray;
+
+            Console.WriteLine("Este Nombre {0}\nDe edad {1}", nombre,edadPeroNumero);
+            
+        }
+    }
+}
